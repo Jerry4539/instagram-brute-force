d8b2942ab9f84f1f71e3e43845e8a3fc0792f714
Jerry_4539 <91048755+Jerry4539@users.noreply.github.com>
2021-12-02 13:59:20 +0530

    Update instagram-brute-force.py

--- instagram-brute-force.py
+++ instagram-brute-force.py
@@ -5,6 +5,21 @@
 from webbot import Browser
 from pynput.keyboard import Key, Controller
 import time
+print(r""" 
+   ___                               ___  _____  _____  _____  
+  |_  |                             /   ||  ___||____ ||  _  | 
+    | | ___ _ __ _ __ _   _ ______ / /| ||___ \     / /| |_| | 
+    | |/ _ \ '__| '__| | | |______/ /_| |    \ \    \ \\____ | 
+/\__/ /  __/ |  | |  | |_| |      \___  |/\__/ /.___/ /.___/ / 
+\____/ \___|_|  |_|   \__, |          |_/\____/ \____/ \____/  
+                       __/ |                                   
+                      |___/                                    
+""")
+    print("\n****************************************************************")
+    print("\n* Copyright of Jerry4539 , 2021                              *")
+    print("\n* https://github.com/Jerry4539                                 *")
+    print("\n* https://www.youtube.com/Anomoyous                          *")
+    print("\n****************************************************************")
 username = input('Username: ')
 dictionary = input('Choose Dictionary: ')
 
